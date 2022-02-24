bd34cdba33ad4ad1cf7d430ab5dea3ce55e8d4d0
BrunoryLaplace <100328719+BrunoryLaplace@users.noreply.github.com>
2022-02-24 10:50:02 +0800

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,11 @@
+package test;
+
+public class hello {
+
+	public static void main(String[] args) {
+		// TODO 自动生成的方法存根
+    System.out.println("hello world!");
+    System.out.print("hello world!");
+	}
+
+}
